bf8307e5e0d559959a32b60f027f96ac53618754
boccorouslan71-svg <boccorouslan71-svg@users.noreply.github.com>
2026-08-27 11:25:59 +0000

    fix: add constructor to AuthorizationRequest stub

--- tools/appauth-stubs/net/openid/appauth/AuthorizationRequest.java
+++ tools/appauth-stubs/net/openid/appauth/AuthorizationRequest.java
@@ -5,6 +5,11 @@ public class AuthorizationRequest {
     public final String codeVerifier;
     public final String state;
     public Map<String, String> additionalParameters;
+    public AuthorizationRequest(String codeVerifier, String state, Map<String, String> additionalParameters) {
+        this.codeVerifier = codeVerifier;
+        this.state = state;
+        this.additionalParameters = additionalParameters;
+    }
     public static class Builder {
         public Builder(AuthorizationServiceConfiguration config, String clientId, String responseType, Uri redirectUri) {}
         public Builder setScopes(String... scopes) { return this; }
